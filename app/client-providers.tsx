fcd922d6221475c05ec2aab7b58571363df978b4
Denzel Kariuki <denzelk741@gmail.com>
2025-06-27 14:07:25 +0300

    Layout reverted to original form

--- app/client-providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import Providers from "@/components/Providers";
-import type { ReactNode } from "react";
-
-export default function ClientProviders({ children }: { children: ReactNode }) {
-  return <Providers>{children}</Providers>;
-}
